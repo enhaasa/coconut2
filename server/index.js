100bd5690cadac1d81b0cfc85b2e16e446ca861f
Enhaasa <enhaasa@pm.me>
2022-12-17 15:49:06 +0100

    Set up server files.

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    user: 'linroot',
+    host: 'lin-13330-7942-mysql-primary.servers.linodedb.net',
+    password: 'nk7PTUbmdz0Xl^62',
+    ssl: {
+        ca: './certs/EnhasicSoftware-ca-certificate.crt'
+    },
+    database: 'menu'
+});
+
+app.listen(3001, () => {
+    console.log("Running");
+})
