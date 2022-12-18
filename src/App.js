9b64934b1703416843d05f699cd3a680c58d52f2
Enhaasa <enhaasa@pm.me>
2022-12-18 17:28:58 +0100

    Added procfile and process.env.PORT.

--- src/App.js
+++ src/App.js
@@ -19,11 +19,11 @@ function App() {
   const loggedInAs = "Coco Shev'rin";
 
   //dbTools.test();
-
+  const port = process.env.PORT || 3001;
 
   const [menu, setMenu] = useState([]);
   useEffect(() => {
-    Axios.get(`http://${document.location.hostname}:3001/menu`).then((res) => {
+    Axios.get(`http://${document.location.hostname}:${port}/menu`).then((res) => {
       res.data.forEach(item => {
        setMenu(prev => (
         [...prev,
