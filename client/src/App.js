e3332206ec0252a1d700655ca57c365fc01a69df
Enhaasa <enhaasa@pm.me>
2022-12-28 19:19:19 +0100

    Change axios URL again

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
   const loggedInAs = "Coco Shev'rin";
 
   const PORT = process.env.PORT || 3001;
-  const baseUrl = process.env.baseURL || "http://localhost:3001"
+  const baseUrl = process.env.BASE_URL || "http://localhost:3001"
 
   const [menu, setMenu] = useState([]);
   useEffect(() => {
